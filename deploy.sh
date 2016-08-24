4eaddfd6a0e67f45463ec40610c103aef6181ad6
amaurs <amaury.gtz@gmail.com>
2016-08-24 15:47:06 -0500

    Page is live.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 OPTIONS="-avzC --exclude-from exclude.txt"
 TRANSPORT='ssh -p 22'
 CONNECTION='amaurs@linode'
-REMOTE_PATH='/var/www/html/mariano'
+REMOTE_PATH='/home/amaurs/www/sites/sancheztalanquer.com'
 
 rsync $OPTIONS -e "$TRANSPORT" . $CONNECTION:$REMOTE_PATH
