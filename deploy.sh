5341ca1f1f20606d993050498579ba8662309550
amaurs <amaury.gtz@gmail.com>
2018-12-18 07:23:37 -0800

    Removed old files.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-OPTIONS="-avzC --exclude-from exclude.txt"
-TRANSPORT='ssh -p 22'
-CONNECTION='amaurs@linode'
-REMOTE_PATH='/home/amaurs/www/sites/sancheztalanquer.com'
-
-rsync $OPTIONS -e "$TRANSPORT" . $CONNECTION:$REMOTE_PATH
